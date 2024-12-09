9e043b749eec9b02f9fd090d8465e499b6b13422
LiemNH <liem.nguyenhoang127@gmail.com>
2024-12-09 22:11:50 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-AUTH0_DOMAIN=test-liemnh.us.auth0.com
-ALGORITHMS=RS256
-API_AUDIENCE=test-coffee-shop.com
-DATABASE_URL=postgresql://postgres:postgres@localhost:5433/capstone
+export AUTH0_DOMAIN="test-liemnh.us.auth0.com"
+export ALGORITHMS="RS256"
+export API_AUDIENCE="test-coffee-shop.com"
+export DATABASE_URL="postgresql://postgres:postgres@localhost:5433/capstone"
